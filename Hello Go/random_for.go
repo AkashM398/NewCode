547bfa2e9424066e9fd58b7c26b0cb4e76f0d1f5
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyDuggempudi@users.noreply.github.com>
2019-02-05 16:58:04 +0530

    Update random_for.go

--- Hello Go/random_for.go
+++ Hello Go/random_for.go
@@ -1,11 +1,13 @@
 package main
 
 import "fmt"
-// Import the math/rand package
 import "math/rand"
+//IMPORT TIME
+import "time"
 
 func main() {
-    
+    rand.Seed(time.Now().Unix())
+    //Specially geneerates random numbers because without this you'll see same series of random numbers generated
     for i := 1; i <= 5; i++ {
         // Generate and print a random integer from 0 to 9
         fmt.Println(rand.Intn(10))
